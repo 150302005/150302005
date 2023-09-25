9458388225a30ebaba20bde79032919a91bd4b81
150302005 <145961546+150302005@users.noreply.github.com>
2023-09-25 12:29:15 +0530

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,9 @@
+hello everyone ...
+The Github is most popular platform where    every one can gather 
+and see the differenet kinds of projects, things or other kind of.
+this platform is good for enhance the technical skill  
+
+<!---
+150302005/150302005 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
